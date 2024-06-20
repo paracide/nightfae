29f0e89289a26fcd6fa9159314cc7e99a82e146e
Arthur <paracidex@gmail.com>
2024-06-19 22:24:21 -0500

    feat(home): scroll page

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,91 +1,80 @@
-import Link from '@/components/Link'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
-import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
+'use client'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import React, { useRef } from 'react'
 
-const MAX_DISPLAY = 5
+export default function Home() {
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start start', 'end start'],
+  })
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ['0%', '100%'])
+  const textY = useTransform(scrollYProgress, [0, 1], ['0%', '200%'])
 
-export default function Home({ posts }) {
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
+      <div ref={ref} className="relative grid h-screen w-full place-items-center overflow-hidden">
+        <motion.h1
+          style={{ y: textY }}
+          className="relative z-10 text-7xl font-bold text-white md:text-9xl"
+        >
+          Nightfae
+        </motion.h1>
+
+        <motion.div
+          className="absolute inset-0 z-0 bg-cover bg-bottom"
+          style={{
+            backgroundImage: `url(static/images/home/bottom.webp)`,
+            y: backgroundY,
+          }}
+        />
+        <div
+          className="absolute inset-0 z-20 bg-cover bg-bottom"
+          style={{
+            backgroundImage: `url(static/images/home/top.webp)`,
+          }}
+        />
+      </div>
+
+      <div className="w-full bg-[#06141D]">
+        <div className="mx-auto max-w-lg space-y-4 py-24 text-neutral-300">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae earum nobis quasi
+            repellat. Amet facere nulla dolorum accusantium sit dolores odio excepturi facilis
+            laboriosam officiis dolorem, nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae earum nobis quasi
+            repellat. Amet facere nulla dolorum accusantium sit dolores odio excepturi facilis
+            laboriosam officiis dolorem, nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae earum nobis quasi
+            repellat. Amet facere nulla dolorum accusantium sit dolores odio excepturi facilis
+            laboriosam officiis dolorem, nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae earum nobis quasi
+            repellat. Amet facere nulla dolorum accusantium sit dolores odio excepturi facilis
+            laboriosam officiis dolorem, nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae earum nobis quasi
+            repellat. Amet facere nulla dolorum accusantium sit dolores odio excepturi facilis
+            laboriosam officiis dolorem, nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae earum nobis quasi
+            repellat. Amet facere nulla dolorum accusantium sit dolores odio excepturi facilis
+            laboriosam officiis dolorem, nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae earum nobis quasi
+            repellat. Amet facere nulla dolorum accusantium sit dolores odio excepturi facilis
+            laboriosam officiis dolorem, nobis reprehenderit molestiae.
           </p>
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
-            return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read more: "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
-        </ul>
       </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="All posts"
-          >
-            All Posts &rarr;
-          </Link>
-        </div>
-      )}
-      {siteMetadata.newsletter?.provider && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
-        </div>
-      )}
     </>
   )
 }
